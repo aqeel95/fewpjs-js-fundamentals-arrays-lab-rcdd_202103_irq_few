a8f1626ab0807b4947959319a5642f219a3ee980
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-04-08 20:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+let append = ["Milo", "Otis", "Garfield", "Odie"];
+consol.log(append);
+
+
+let prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift("Odie");
+consol.log(prepend);
